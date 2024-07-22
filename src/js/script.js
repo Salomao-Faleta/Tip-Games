dd9e3cefee7a1f3da70d344b8aba6f6160d4e526
Salomao-Faleta <faletas41@gmail.com>
2024-07-21 22:59:22 -0300

    finalizando o slide

--- src/js/script.js
+++ src/js/script.js
@@ -10,10 +10,41 @@ document.querySelector('.menu-oppener').addEventListener('click', ()=>{
 // SEÇÃO SOBRE - SLIDER
 
 // Definindo a largura do item dinâmicamenete
-let sliderItem = document.querySelectorAll(".slider--item").length;
-document.querySelector('.slider--width').style.width = `calc(100vw * ${sliderItem})`;
+let totalSlides = document.querySelectorAll(".slider--item").length;
+document.querySelector('.slider--width').style.width = `calc(100vw * ${totalSlides})`;
 
 
 // Definindo a altura do item dinâmicamenete
 let slider = document.querySelector('.slider');
 let sliderControls = document.querySelector('.slider--controls').style.height = `${slider.clientHeight}px`
+
+
+//FUNÇÕES PARA MUDAR DE IMAGEM 
+
+let currentSlide = 0;
+
+function goPrev(){
+    currentSlide--;
+    if(currentSlide < 0){
+        currentSlide = totalSlides - 1
+    }
+
+    updateMargin();
+}
+
+function goNext(){
+    currentSlide++;
+    if(currentSlide > (totalSlides - 1)){
+        currentSlide = 0
+    }
+    updateMargin();
+}
+
+
+//Função que faz a "mágica" acontecer
+function updateMargin(){
+    let newMargin = (currentSlide * document.body.clientWidth);
+
+    document.querySelector('.slider--width').style.marginLeft = `-${newMargin}px`
+
+}

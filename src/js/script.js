5a2354d0c5eb3afa16f23c69e5bf88cc1a66381e
Salomao-Faleta <faletas41@gmail.com>
2024-07-21 22:43:49 -0300

    definindo altura e largura do control

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,19 @@
+// MENU
 document.querySelector('.menu-oppener').addEventListener('click', ()=>{
     let nav = document.querySelector('header nav');
     nav.classList.toggle('oppened');
 });
+
+
+
+
+// SEÇÃO SOBRE - SLIDER
+
+// Definindo a largura do item dinâmicamenete
+let sliderItem = document.querySelectorAll(".slider--item").length;
+document.querySelector('.slider--width').style.width = `calc(100vw * ${sliderItem})`;
+
+
+// Definindo a altura do item dinâmicamenete
+let slider = document.querySelector('.slider');
+let sliderControls = document.querySelector('.slider--controls').style.height = `${slider.clientHeight}px`
